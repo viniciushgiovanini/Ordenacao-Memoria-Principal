2bb612cd7f8a62fdf2cbe7ab2f59114dccc6aa35
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-06-28 02:21:13 -0300

    Iniciando HeapSort Numerico Java
    Inciando heapsort

---
+++ heapsort/heapNum.java
@@ -0,0 +1,88 @@
+package heapsort;
+
+class heapNum {
+
+  // FUNCOES DE APOIO EM CIMA
+  private static void swap(int[] vetor, int posi1, int posi2) {
+
+    int tmp;
+
+    tmp = vetor[posi1];
+    vetor[posi1] = vetor[posi2];
+    vetor[posi2] = tmp;
+
+  }
+
+  // FUNCOES DO HEAP
+
+  public static void heapsort(int[] array) {
+    int n = array.length;
+    // Alterar o vetor ignorando a posicao zero
+    int[] tmp = new int[n + 1];
+    for (int i = 0; i < n; i++) {
+      tmp[i + 1] = array[i];
+    }
+    array = tmp;
+
+    // Contrucao do heap
+    for (int tamHeap = 2; tamHeap <= n; tamHeap++) {
+      construir(tamHeap, array);
+    }
+
+    // Ordenacao propriamente dita
+    int tamHeap = n;
+    while (tamHeap > 1) {
+      swap(array, 1, tamHeap--);
+      reconstruir(tamHeap, array);
+    }
+
+    // Alterar o vetor para voltar a posicao zero
+    tmp = array;
+    array = new int[n];
+    for (int i = 0; i < n; i++) {
+      array[i] = tmp[i + 1];
+    }
+  }
+
+  public static void construir(int tamHeap, int[] array) {
+    for (int i = tamHeap; i > 1 && array[i] > array[i / 2]; i /= 2) {
+      swap(array, i, i / 2);
+    }
+  }
+
+  public static void reconstruir(int tamHeap, int[] array) {
+    int i = 1;
+    while (i <= (tamHeap / 2)) {
+      int filho = getMaiorFilho(i, tamHeap, array);
+      if (array[i] < array[filho]) {
+        swap(array, i, filho);
+        i = filho;
+      } else {
+        i = tamHeap;
+      }
+    }
+  }
+
+  public static int getMaiorFilho(int i, int tamHeap, int[] array) {
+    int filho;
+    if (2 * i == tamHeap || array[2 * i] > array[2 * i + 1]) {
+      filho = 2 * i;
+    } else {
+      filho = 2 * i + 1;
+    }
+    return filho;
+  }
+
+  public static void main(String[] args) {
+    int vetor[] = { 100, 200, 300, 40, 30, 25, 1, 45, 0, 8, 5, 2, 6, 1, 0 };
+
+    heapsort(vetor);
+
+    for (int i = 0; i < vetor.length; i++) {
+
+      System.out.println(vetor[i]);
+
+    }
+  }
+
+}
